45090e38ebd6ca92544b19bb0e314f0add2d6d70
tweoss <hwymonkey@gmail.com>
2020-12-23 15:41:32 -0800

    fixed for grayscale

--- temp.js
+++ temp.js
@@ -1,5 +1,72 @@
 'use strict';
 
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+function rgbToHsv(r, g, b) {
+	r /= 255, g /= 255, b /= 255;
+
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, v = max;
+
+	var d = max - min;
+	s = max == 0 ? 0 : d / max;
+
+	if (max == min) {
+		h = 0; // achromatic
+	} else {
+		switch (max) {
+			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / d + 2; break;
+			case b: h = (r - g) / d + 4; break;
+		}
+
+		h /= 6;
+	}
+
+	return [h, s, v];
+}
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+function hsvToRgb(h, s, v) {
+	var r, g, b;
+
+	var i = Math.floor(h * 6);
+	var f = h * 6 - i;
+	var p = v * (1 - s);
+	var q = v * (1 - f * s);
+	var t = v * (1 - (1 - f) * s);
+
+	switch (i % 6) {
+		case 0: r = v, g = t, b = p; break;
+		case 1: r = q, g = v, b = p; break;
+		case 2: r = p, g = v, b = t; break;
+		case 3: r = p, g = q, b = v; break;
+		case 4: r = t, g = p, b = v; break;
+		case 5: r = v, g = p, b = q; break;
+	}
+
+	return [r * 255, g * 255, b * 255];
+}
+
 
 //? 1
 var drawing_canvas = document.createElement("canvas");
@@ -41,7 +108,18 @@ function messaged({data: {points, outputrgba}}) {
 		// x = Math.floor(x); y = Math.floor(y);
 		// // drawing_context.fillStyle = "#" + rgba[x + y * width] + rgba[x + y * width + 1] + rgba[x + y * width + 2];
 		let index = (i / 2 * 3);
-		drawing_context.fillStyle = `rgb(${Math.floor(outputrgba[index])},${Math.floor(outputrgba[index + 1])},${Math.floor(outputrgba[index + 2])})`;
+		let [hue, saturation, value] = rgbToHsv(Math.floor(outputrgba[index]),Math.floor(outputrgba[index + 1]),Math.floor(outputrgba[index + 2]));
+		// console.log(saturation);
+		let r,g,b;
+		if (saturation == 0) {
+			r = value * 255; g = value * 255; b = value * 255;
+		}
+		else {
+			[r,g,b] = hsvToRgb(hue, 1. , value);
+		}
+		// c.fillStyle = 'hsv('+ 360*Math.random() +',100%,50%)';
+		// let [r,g,b] = hsvToRgb(hue, saturation ? 1. : 0., value);
+		drawing_context.fillStyle = `rgb(${r},${g},${b})`;
 		// drawing_context.fillStyle = "#000";
 		drawing_context.fill();
 		drawing_context.beginPath();
@@ -72,14 +150,14 @@ image.addEventListener('load', function() {
 	// for (let i = 0, n = rgba.length / 4; i < n; ++i) rgba[i * 4] = Math.max(0, 1 - rgba[i * 4] / 254);
 	//! data now contains the weighting
 	//! floatrgba SHOULD not be touched
+	// let n = Math.round(width * height / 600);
 	let n = Math.round(width * height / 40);
-	// let n = Math.round(width * height / 100);
 	const worker = new Worker('worker.js');
 	worker.addEventListener("message", messaged);
 	worker.postMessage({rgba, width, height, n});
 	// worker.postMessage({floatrgba, width, height, n});
 }, false);
-image.src = './imgs/obama.png'; // Set source path
+image.src = './imgs/IMG_2319 copy.jpg'; // Set source path
 
 
 
