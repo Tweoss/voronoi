47c8d9b94fad1b56a6863f5ed23793e9420ae87a
tweoss <hwymonkey@gmail.com>
2020-12-23 19:35:44 -0800

    sample pic

--- index.js
+++ index.js
@@ -129,88 +129,10 @@ image.addEventListener('load', function() {
 	const {data: rgba} = context.getImageData(0, 0, width, height);
 	
 	//* n is the number of points
-	let n = Math.round(width * height / 400);
+	let n = Math.round(width * height / 40);
 	const worker = new Worker('worker.js');
 	worker.addEventListener("message", messaged);
 	worker.postMessage({rgba, width, height, n});
 	// worker.postMessage({floatrgba, width, height, n});
 }, false);
-image.src = './imgs/obama.png'; // Set source path
-
-
-
-
-
-
-
-	// 	main.variable(observer("script")).define("script", ["require","invalidation"], async function(require,invalidation)
-	// {
-	// 	const blob = new Blob([`
-	// importScripts("${await require.resolve("d3-delaunay@^5.1.1")}");
-	
-	// onmessage = event => {
-	// 	const {data: {data, width, height, n}} = event;
-	// 	const points = new Float64Array(n * 2);
-	// 	const c = new Float64Array(n * 2);
-	// 	const s = new Float64Array(n);
-	
-	// 	// Initialize the points using rejection sampling.
-	// 	for (let i = 0; i < n; ++i) {
-	// 		for (let j = 0; j < 30; ++j) {
-	// 			const x = points[i * 2] = Math.floor(Math.random() * width);
-	// 			const y = points[i * 2 + 1] = Math.floor(Math.random() * height);
-	// 			if (Math.random() < data[y * width + x]) break;
-	// 		}
-	// 	}
-	
-	// 	const delaunay = new d3.Delaunay(points);
-	// 	const voronoi = delaunay.voronoi([0, 0, width, height]);
-	
-	// 	for (let k = 0; k < 80; ++k) {
-	
-	// 		// Compute the weighted centroid for each Voronoi cell.
-	// 		c.fill(0);
-	// 		s.fill(0);
-	// 		for (let y = 0, i = 0; y < height; ++y) {
-	// 			for (let x = 0; x < width; ++x) {
-	// 				const w = data[y * width + x];
-	// 				i = delaunay.find(x + 0.5, y + 0.5, i);
-	// 				s[i] += w;
-	// 				c[i * 2] += w * (x + 0.5);
-	// 				c[i * 2 + 1] += w * (y + 0.5);
-	// 			}
-	// 		}
-	
-	// 		// Relax the diagram by moving points to the weighted centroid.
-	// 		// Wiggle the points a little bit so they don’t get stuck.
-	// 		const w = Math.pow(k + 1, -0.8) * 10;
-	// 		for (let i = 0; i < n; ++i) {
-	// 			const x0 = points[i * 2], y0 = points[i * 2 + 1];
-	// 			const x1 = s[i] ? c[i * 2] / s[i] : x0, y1 = s[i] ? c[i * 2 + 1] / s[i] : y0;
-	// 			points[i * 2] = x0 + (x1 - x0) * 1.8 + (Math.random() - 0.5) * w;
-	// 			points[i * 2 + 1] = y0 + (y1 - y0) * 1.8 + (Math.random() - 0.5) * w;
-	// 		}
-	
-	// 		postMessage(points);
-	// 		voronoi.update();
-	// 	}
-	
-	// 	close();
-	// };
-	// `], {type: "text/javascript"});
-	// 	const script = URL.createObjectURL(blob);
-	// 	// invalidation.then(() => URL.revokeObjectURL(script));
-	// 	return script;
-	// }
-	// );
-	// 	main.variable(observer("data")).define("data", ["FileAttachment","width","DOM"], async function(FileAttachment,width,DOM)
-	// {
-		
-	// }
-	// );
-	// 	main.variable(observer("n")).define("n", ["width","height"], function(width,height){return(
-	// )});
-	// 	main.variable(observer("height")).define("height", ["data"], function(data){return(
-	// )});
-		// return main;
-	
+image.src = './imgs/IMG_1886.JPG'; // Set source path
