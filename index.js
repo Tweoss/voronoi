1c8373324ab15502a086b7bb1fa0231ed7824ce7
Francis Chua <francis@opennetworking.org>
2021-10-26 18:37:16 -0700

    url fix

--- index.js
+++ index.js
@@ -233,5 +233,5 @@ document.addEventListener("DOMContentLoaded", function() {
         restart_and_write_url();
     });
 
-    document.getElementById("output-url").value = window.location.host + "/?json=" + encodeURIComponent(btoa(JSON.stringify(settings)));
+    document.getElementById("output-url").value = window.location.replace(/\/$/, '') + "/?json=" + encodeURIComponent(btoa(JSON.stringify(settings)));
 });
